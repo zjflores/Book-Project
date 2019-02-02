6be3b40337ead387573d223fbc00623738a221c7
zjflores <zsasha.flores@gmail.com>
2019-02-02 04:02:22 -0800

    Add docstrings and organize server

--- backend/server.py
+++ backend/server.py
@@ -7,9 +7,15 @@ from flask_cors import CORS, cross_origin
 from model import User, Book, BookUser, Genre, BookGenre, connect_to_db, db
 from data.keys.secret_keys import flask
 from random import choice
+
+################################################################################
+################################# SETUP ####################################### 
+################################################################################
+
 app = Flask(__name__)
 cors = CORS(app, resources={
     r"/*": {r"supports_credentials": True, r"origins": r"http://localhost:3000"}})
+
 # Required to use Flask sessions and the debug toolbar
 app.secret_key = flask.secret
 
@@ -21,9 +27,45 @@ def after(response):
     return response
 
 
+################################################################################
+############################## GENERAL ROUTES ##################################
+################################################################################
+
+
+@app.route('/get-title', methods=['POST'])
+@cross_origin()
+def get_title():
+    data = request.get_json()
+    print(data)
+
+    book = Book.query.get(data["id"])
+    return jsonify(book.title)
+
+
+@app.route('/get-name', methods=['POST'])
+@cross_origin()
+def get_username():
+    data = request.get_json()
+    print(data)
+
+    user = User.query.get(data["id"])
+    return jsonify(user.fname)
+
+
+@app.route('/get-userid', methods=['GET'])
+@cross_origin()
+def get_userid():
+    print(session)
+    return jsonify(session['user_id'])
+
+
+################################################################################
+############################## HOME ROUTES #################################### 
+################################################################################
+
 @app.route('/')
 def index():
-    """Homepage"""
+    """Randomly choose quotation rendered on homepage"""
 
     quotes = [
         "“If you go home with somebody, and they don't have books, don't fuck 'em!” - John Waters", 
@@ -80,7 +122,7 @@ def register():
 @app.route('/login', methods=['POST'])
 @cross_origin()
 def login():
-    """validates user's login info"""
+    """Validates user's login info"""
     data = request.get_json()
     print(data)
 
@@ -108,113 +150,11 @@ def logout():
     return jsonify("User has been logged out")
 
 
-@app.route('/get-title', methods=['POST'])
-@cross_origin()
-def get_title():
-    data = request.get_json()
-    print(data)
+################################################################################
+############################## BOOK ROUTES #################################### 
+################################################################################
 
-    book = Book.query.get(data["id"])
-    return jsonify(book.title)
-
-
-@app.route('/get-name', methods=['POST'])
-@cross_origin()
-def get_username():
-    data = request.get_json()
-    print(data)
-
-    user = User.query.get(data["id"])
-    return jsonify(user.fname)
-
-
-@app.route('/get-userid', methods=['GET'])
-@cross_origin()
-def get_userid():
-    print(session)
-    return jsonify(session['user_id'])
-
-
-@app.route('/get-genres', methods=['GET'])
-@cross_origin()
-def get_genres():
-    "Query db for genres"
-
-    genres = []
-    q = Genre.query.all()
-    for i in q:
-        genres.append({'text': i.genre, 'value': i.id})
-
-    return jsonify(genres)
-
-
-@app.route('/get-bookgenres', methods=['POST'])
-@cross_origin()
-def get_bookgenres():
-    data = request.get_json()
-    print(data)
-
-    genres = []
-    q = BookGenre.query.filter(BookGenre.book_id == data['id'])
-
-
-    for i in q.all():
-        genre = Genre.query.get(i.genre_id)
-        genres.append({'genre': genre.genre})
-    print(genres)
-    return jsonify(genres)
-
-  
-
-
-@app.route('/set-genres', methods=['POST'])
-@cross_origin()
-def set_genres():
-    """Adds new BookGenre entries"""
-
-    print("Touched server")
-    data = request.get_json()
-    print(data)
-
-    for genre in data["genres"]:
-        new_book_genre = BookGenre(book_id=data["id"], genre_id=genre["value"])
-        db.session.add(new_book_genre)
-    db.session.commit()
-    return jsonify("Genres added")
-
-
-@app.route('/get-users', methods=['GET'])
-@cross_origin()
-def get_users():
-    """Return list of users from db."""
-
-    users = []
-    print("session id: {}".format(session['user_id']))
-
-    q = User.query.all()
-    print(q)
-    for user in q:
-        if user.id != session['user_id']:
-            users.append({'name': user.fname, 'id': user.id})
-    return jsonify(users)
-
-
-@app.route('/get-user-books', methods=['POST'])
-@cross_origin()
-def get_user_books():
-    """Query db for a user's books"""
-    data = request.get_json()
-    print(data)
-
-    books = []
-
-    q = BookUser.query.filter(BookUser.user_id == data['id']).all()
-    for i in q:
-        book = Book.query.get(i.book_id)
-        books.append(
-            {'title': book.title, 'author': book.author, 'id': book.id})
-    print(books)
-    return jsonify(books)
+############################ BOOK CRUD CYCLE ###################################
 
 
 @app.route('/get-your-books', methods=['GET'])
@@ -238,7 +178,7 @@ def get_your_books():
 @app.route('/add-book', methods=['POST'])
 @cross_origin()
 def add_book():
-    """Add book to db"""
+    """Add new book to db"""
 
     data = request.get_json()
     print(data)
@@ -348,9 +288,76 @@ def update_book():
             return jsonify(book)
 
 
+############################ ADDITIONAL BOOK PATHS #############################
+################################## GENRE #######################################
+
+
+@app.route('/get-genres', methods=['GET'])
+@cross_origin()
+def get_genres():
+    "Query db for all genres"
+
+    genres = []
+    q = Genre.query.all()
+    for i in q:
+        genres.append({'text': i.genre, 'value': i.id})
+
+    return jsonify(genres)
+
+
+@app.route('/get-bookgenres', methods=['POST'])
+@cross_origin()
+def get_bookgenres():
+    """Get all genres associated with a book's id"""
+
+    data = request.get_json()
+    print(data)
+
+    genres = []
+    q = BookGenre.query.filter(BookGenre.book_id == data['id'])
+
+    for i in q.all():
+        genre = Genre.query.get(i.genre_id)
+        genres.append({'genre': genre.genre})
+    print(genres)
+    return jsonify(genres)
+
+
+@app.route('/set-genres', methods=['POST'])
+@cross_origin()
+def set_genres():
+    """Adds new BookGenre entries"""
+
+    data = request.get_json()
+    print(data)
+
+    for genre in data["genres"]:
+        new_book_genre = BookGenre(book_id=data["id"], genre_id=genre["value"])
+        db.session.add(new_book_genre)
+    db.session.commit()
+    return jsonify("Genres added")
+
+
+################################### DATES ######################################
+
+
+@app.route('/get-start-date', methods=['POST'])
+@cross_origin()
+def get_start_date():
+    """Query db for reading date user began reading a title"""
+
+    data = request.get_json()
+    print(data)
+
+    book_user = BookUser.query.filter((BookUser.book_id==data['book_id']) & (BookUser.user_id==data['id'])).one()
+    return jsonify(book_user.start_date)
+
+
 @app.route('/book/set-start-date', methods=['POST'])
 @cross_origin()
 def set_start_date():
+    """Add date user began reading title to db"""
+
     data = request.get_json()
     print(data)
 
@@ -360,9 +367,23 @@ def set_start_date():
     return jsonify("Start date added")
 
 
+@app.route('/get-end-date', methods=['POST'])
+@cross_origin()
+def get_end_date():
+    """Query db for date user finished reading a title"""
+
+    data = request.get_json()
+    print(data)
+
+    book_user = BookUser.query.filter((BookUser.book_id==data['book_id']) & (BookUser.user_id==data['id'])).one()
+    return jsonify(book_user.end_date)
+
+
 @app.route('/book/set-end-date', methods=['POST'])
 @cross_origin()
 def set_end_date():
+    """Add date user completed reading a title to db"""
+
     data = request.get_json()
     print(data)
 
@@ -371,34 +392,16 @@ def set_end_date():
     db.session.commit()
     return jsonify("End date added")
 
-
-@app.route('/get-start-date', methods=['POST'])
-@cross_origin()
-def get_start_date():
-    data = request.get_json()
-    print(data)
-
-    book_user = BookUser.query.filter((BookUser.book_id==data['book_id']) & (BookUser.user_id==data['id'])).one()
-    return jsonify(book_user.start_date)
-
-
-@app.route('/get-end-date', methods=['POST'])
-@cross_origin()
-def get_end_date():
-    data = request.get_json()
-    print(data)
-    book_user = BookUser.query.filter((BookUser.book_id==data['book_id']) & (BookUser.user_id==data['id'])).one()
-    return jsonify(book_user.end_date)
-
-
+############################ BOOK CRUD CYCLE ###################################
 @app.route('/get-readers', methods=['POST'])
 @cross_origin()
 def get_readers():
+    """Query db for other users reading title"""
+
     data = request.get_json()
     print(data)
 
     readers = []
-
     q = BookUser.query.filter(BookUser.book_id==data['book_id'])
 
     if q.count() > 1:
@@ -413,6 +416,8 @@ def get_readers():
 @app.route('/get-authorization', methods=['POST'])
 @cross_origin()
 def get_authorization():
+    """Confirm url param matches id in session"""
+
     data = request.get_json()
     print(data)
 
@@ -420,6 +425,46 @@ def get_authorization():
         return jsonify("woo"), status.HTTP_200_OK
     else:
         return jsonify("nope"), status.HTTP_401_UNAUTHORIZED
+
+
+################################################################################
+############################## USER ROUTES #################################### 
+################################################################################
+
+
+@app.route('/get-users', methods=['GET'])
+@cross_origin()
+def get_users():
+    """Return list of users from db."""
+
+    users = []
+    print("session id: {}".format(session['user_id']))
+
+    q = User.query.all()
+    print(q)
+    for user in q:
+        if user.id != session['user_id']:
+            users.append({'name': user.fname, 'id': user.id})
+    return jsonify(users)
+
+
+@app.route('/get-user-books', methods=['POST'])
+@cross_origin()
+def get_user_books():
+    """Query db for a user's books"""
+
+    data = request.get_json()
+    print(data)
+
+    books = []
+    q = BookUser.query.filter(BookUser.user_id == data['id']).all()
+    
+    for i in q:
+        book = Book.query.get(i.book_id)
+        books.append(
+            {'title': book.title, 'author': book.author, 'id': book.id})
+    print(books)
+    return jsonify(books)
 
 
 if __name__ == "__main__":
