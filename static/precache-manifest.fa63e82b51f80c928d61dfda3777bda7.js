47434e73d2143f5a4bdb33ce1982832f165d2a46
zjflores <zsasha.flores@gmail.com>
2019-08-08 23:35:16 -0700

    move everything into one app so it can be a SPA with a backend

---
+++ static/precache-manifest.fa63e82b51f80c928d61dfda3777bda7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ff1ba7a0b7bdabdd67c",
+    "url": "/static/css/main.54ed0729.chunk.css"
+  },
+  {
+    "revision": "1ff1ba7a0b7bdabdd67c",
+    "url": "/static/js/main.1ff1ba7a.chunk.js"
+  },
+  {
+    "revision": "784150fd13df1eaf1104",
+    "url": "/static/js/1.784150fd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8bdf3df1968ba467bb36a0e11d3d1a26",
+    "url": "/index.html"
+  }
+];
---
+++ static/precache-manifest.fb3e234438f196d51ecfc23aab96a217.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d555da0bfafab7440a2a9707a4c1180",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95bbf25bf81de31b24be",
+    "url": "/static/css/main.2fe8e291.chunk.css"
+  },
+  {
+    "revision": "5f1c2264121d7422a448",
+    "url": "/static/js/2.e405af85.chunk.js"
+  },
+  {
+    "revision": "95bbf25bf81de31b24be",
+    "url": "/static/js/main.f83403a2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
